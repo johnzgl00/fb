5320104224d271f06e1551767cf70b70526c4859
John Zygoulakis <67337564+johnzgl00@users.noreply.github.com>
2022-07-13 22:05:14 +0100

    Add files via upload

--- Filament Box/main.py
+++ Filament Box/main.py
@@ -1,9 +1,9 @@
-from cgitb import grey
 import tkinter as tk
 
 window = tk.Tk()
 window.geometry("480x320")
 window.title("FILAMENT BOX")
+window.configure(bg="red")
 
 btnheight=10
 btnwidth=22
@@ -17,22 +17,22 @@ controlbtnImg=tk.PhotoImage(file="controlbtn/controlbtn1.png")
 aboutbtnImg=tk.PhotoImage(file="aboutbtn/aboutbtn1.png")
 buttonImg=tk.PhotoImage(file="button.png")
 
-statusbtn = tk.Button(window, image=statusbtnImg, borderwidth=0, bg=backgroundColor, activebackground=backgroundColor)
+statusbtn = tk.Button(window, image=statusbtnImg, bd=0, highlightthickness=0, bg=backgroundColor, activebackground=backgroundColor)
 statusbtn.grid(column=0,row=0)
 
-confbtn = tk.Button(window, image=confbtnImg, borderwidth=0, bg=backgroundColor, activebackground=backgroundColor)
+confbtn = tk.Button(window, image=confbtnImg, bd=0, highlightthickness=0, bg=backgroundColor, activebackground=backgroundColor)
 confbtn.grid(column=1,row=0)
 
-infobtn = tk.Button(window, image=infobtnImg, borderwidth=0, bg=backgroundColor, activebackground=backgroundColor)
+infobtn = tk.Button(window, image=infobtnImg, bd=0, highlightthickness=0, bg=backgroundColor, activebackground=backgroundColor)
 infobtn.grid(column=2,row=0)
 
-systembtn = tk.Button(window, image=sysbtnImg, borderwidth=0, bg=backgroundColor, activebackground=backgroundColor)
+systembtn = tk.Button(window, image=sysbtnImg, bd=0, highlightthickness=0, bg=backgroundColor, activebackground=backgroundColor)
 systembtn.grid(column=0,row=1)
 
-controlbtn = tk.Button(window, image=controlbtnImg, borderwidth=0, bg=backgroundColor, activebackground=backgroundColor)
+controlbtn = tk.Button(window, image=controlbtnImg, bd=0, highlightthickness=0, bg=backgroundColor, activebackground=backgroundColor)
 controlbtn.grid(column=1,row=1)
 
-aboutbtn = tk.Button(window, image=aboutbtnImg, borderwidth=0, bg=backgroundColor, activebackground=backgroundColor)
+aboutbtn = tk.Button(window, image=aboutbtnImg, bd=0, highlightthickness=0, bg=backgroundColor, activebackground=backgroundColor)
 aboutbtn.grid(column=2,row=1)
 
 
